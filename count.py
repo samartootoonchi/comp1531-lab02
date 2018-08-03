6710acb5b62209855f3f21e5f7da75c0b8ccb2d7
Samar Tootoonchi <z5125690@student.unsw.edu.au>
2018-08-03 18:41:45 +1000

    ADDED FINAL CHANGE

--- count.py
+++ count.py
@@ -9,23 +9,59 @@
 
 
 def count_char(text):
-    pass
-    # add your code here
+    dicationary = {}
+
+    for i in text:
+        if i in dicationary:
+            dicationary[i] += 1   
+        else:
+            dicationary[i] = 1  
+            
+    for i in dicationary:
+        print(i,dicationary[i])  
+    
+    
 
 def count_char_insensitive(text):
-    pass
-    # add your code here
+    dicationary = {}
+    text = text.lower()
+
+    for i in text:
+        if i in dicationary:
+            dicationary[i] += 1   
+        else:
+            dicationary[i] = 1  
+            
+    for i in dicationary:
+        print(i,dicationary[i])  
+
+
 
 # bonus task:
 def count_char_ordered(text):
-    pass
-    # add your code here 
+    dicationary = {}
+    text = text.lower()
 
+    for i in text:
+        if i in dicationary:
+            dicationary[i] += 1   
+        else:
+            dicationary[i] = 1  
+            
+
+        
+    dicationary.items()  
+    result = sorted(dicationary.items(), key = lambda tup:tup[1],reverse = True)
+
+    for i in result:
+        print(i[0],i[1])
+            
 text1 = 'I felt happy because I saw the others were happy and because I knew I should feel happy but I wasn’t really happy' # Robert Bolano
 text2 = 'HellO, WorLd!'
 
 # testing exercise 2
 count_char(text2)
 count_char_insensitive(text2)
+print()
 count_char_ordered(text2)
 
