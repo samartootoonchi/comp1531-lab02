d9cc827a96dfc3a2092b04ef34164352a05985e1
Samar Tootoonchi <z5125690@student.unsw.edu.au>
2018-08-03 17:34:37 +1000

    ADDED FINAL CHANGE

--- number_list.py
+++ number_list.py
@@ -1,31 +1,32 @@
 def find_reverse(numbers):
-    #TODO: find the reverse of the list
-    pass
-
+    return numbers[::-1]
+    
+    
 def find_max(numbers):
-    #TODO: find the maximum of the list
-    pass
-
+    return max(numbers)
+    
+    
 def find_min(numbers):
-    #TODO: find the minimum of the list
-    pass
-
+    return min(numbers)
+    
+    
 def find_sum(numbers):
-    #TODO: find the sum of all the numbers in the list
-    pass
-
+    return sum(numbers)
+    
+    
 def find_average(numbers):
-    #TODO: find the average of all the numbers in the list
-    pass
-
+    return sum(numbers)/ len(numbers)
+    
+    
 def find_descending(numbers):
-    #TODO: numbers sorted in descending order
-    pass
+    return sorted(numbers, reverse = True)
+    
 
 def second_smallest(numbers):
-    #TODO: find the second smallest
-    pass
-
+    new_set = set(numbers)
+    new_list = list(new_set)
+    return sorted(new_list)[1] 
+    
 
 '''
  bonus task:
@@ -33,5 +34,6 @@ def second_smallest(numbers):
  and prints the kth smallest number in the list
 '''
 def kth_smallest(numbers, k):
-    #TODO: find the kth smallest number in the list
-    pass
+    new_set = set(numbers)
+    new_list = list(new_set)
+    return sorted(new_list)[k-1] 
